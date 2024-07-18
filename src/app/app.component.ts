f0d9f8392460bf2730ca5e196fce2055bd2fe4b1
Peter Benikovszky <peter@delcom.nl>
2024-07-18 14:58:37 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { FormBuilder, FormGroup } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'awesome-pdr-reader';
+export class AppComponent implements OnInit {
+  public selectedFile: File
+  public form: FormGroup
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      dateRange: [[null, null], []]
+    })
+  }
+
+  public onFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement
+    if (input.files && input.files.length > 0) {
+      this.selectedFile = input.files[0]
+    }
+  }
+
+  public async onSubmit() {
+    if (this.selectedFile) {
+      const arrayBuffer = await this.selectedFile.arrayBuffer()
+      const pdf = await (window as any).pdfjsLib.getDocument({
+        data: arrayBuffer
+      }).promise
+      const maxPages = pdf.numPages
+
+      const words = []
+      for (let pageNum = 1; pageNum <= maxPages; pageNum++) {
+        const page = await pdf.getPage(pageNum)
+        const content = await page.getTextContent()
+        words.push(
+          content.items
+            .map((item) => item.str)
+            .filter((word: string) => word?.length > 0 && word != ' ')
+        )
+      }
+      console.log(words)
+    } else {
+      console.log('No file selected.')
+    }
+  }
 }
